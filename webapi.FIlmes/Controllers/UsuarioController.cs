316ffcc05104a7cb1c3453ef8532b037d014fdc3
juliaathar <juathar@gmail.com>
2023-09-04 10:46:01 -0300

    gerando token

--- webapi.FIlmes/Controllers/UsuarioController.cs
+++ webapi.FIlmes/Controllers/UsuarioController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using webapi.FIlmes.Domains;
 using webapi.FIlmes.Interfaces;
 using webapi.FIlmes.Repositories;
@@ -28,19 +31,65 @@ namespace webapi.FIlmes.Controllers
             {
                 UsuarioDomain usuario = _usuarioRepository.Login(Email, Senha) ;
 
-                if (usuario != null)
+                if (usuario == null)
                 {
-                    return Ok(usuario);
+                    return NotFound("Email ou senha inválidos");
                 }
-                else
+
+                // caso encontre o usuario, prossegue para a criacao do token
+
+                //1 - definir as informacoes(claims) que serao fornecidos no token (payload)
+
+                var claims = new[]
                 {
-                    return Unauthorized();
-                }
+                    new Claim(JwtRegisteredClaimNames.Jti, usuario.IdUsuario.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, usuario.Email),
+                    new Claim(ClaimTypes.Role, usuario.Permissao), 
+
+                    //existe a possibilidade de criar uma claim personalizada
+                    new Claim("ClaimPersonalizada", "Valor da Claim Personalizada")
+                };
+
+                //2 - definir a chave de acesso ao token
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("filmes-chave-autenticacao-webapi-dev"));
+
+                // 3 - definir as credenciais do token (header)
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                //4 - gerar o token
+
+                var token = new JwtSecurityToken
+                    (
+                       //emissor do token
+                       issuer: "webapi.FIlmes",
+
+                       //destinatario do token
+                       audience: "webapi.FIlmes",
+
+                       //dados definidos nas claims(informacoes)
+                       claims: claims,
+
+                       //tempo de expiracao do token
+                       expires: DateTime.Now.AddMinutes(5),
+
+                       //credencias do token
+                       signingCredentials: creds
+
+                    );
+
+                //5- retornar o token criado
+                return Ok(new{
+
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
             }
             catch (Exception erro)
             {
                 return BadRequest(erro.Message);
             }
+            return Ok();
         }
     }
 }
